0168e08138f43913eeea91807a207dbcc6fce129
muhammad nur rahman <138513675+sarrahman-me@users.noreply.github.com>
2025-03-25 16:19:26 +0700

    :hammer: menambahkan vercel analystic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const roboto = Roboto({
@@ -23,6 +24,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
